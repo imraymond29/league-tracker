44cdbc9852a0c2bec507f5fb1fe9a7603336b215
Raymond Lam <raymondlam197@gmail.com>
2017-09-03 23:24:42 -0400

    small commit

--- app/src/main/java/imraymond29/leaguetracker/MainActivity.java
+++ app/src/main/java/imraymond29/leaguetracker/MainActivity.java
@@ -1,7 +1,10 @@
 package imraymond29.leaguetracker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+
+import android.view.View;
 
 /**
  * Created by raymondlam on 2017-08-31.
@@ -12,8 +15,15 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(new MainView(this));
 
+    }
+
+    public class MainView extends View {
+        public MainView(Context context) {
+            super(context);
+
+        }
     }
 
 }
